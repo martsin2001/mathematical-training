8eedcf634f22c5502365ef5e2b224c9e30392071
Myroslav Martsin <forvaro@gmail.com>
2019-11-06 14:08:37 +0200

    unstash changes

--- src/app/core/models/app.models.ts
+++ src/app/core/models/app.models.ts
@@ -1,3 +1,13 @@
+export interface Action {
+  selectedAction: boolean;
+  numbersRange?: {
+    from: number;
+    to: number;
+  };
+  icon?: any;
+  quantityNumbers?: number;
+}
+
 export enum SelectedAction {
   plus = 'plus',
   minus = 'minus',
