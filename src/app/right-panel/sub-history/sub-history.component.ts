05d62b5447fcb5643b86bcf22ecebcfe2b3f5dd3
Myroslav Martsin <forvaro@gmail.com>
2019-11-07 10:13:51 +0200

    add minor changes

--- src/app/right-panel/sub-history/sub-history.component.ts
+++ src/app/right-panel/sub-history/sub-history.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { SubHistory } from 'src/app/core/models/app.models';
 
 @Component({
   selector: 'app-sub-history',
   templateUrl: './sub-history.component.html',
-  styleUrls: ['./sub-history.component.scss']
+  styleUrls: ['./sub-history.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SubHistoryComponent implements OnInit {
   @Input() subHistory: SubHistory;
